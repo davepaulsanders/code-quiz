9a16999c4f0bf2a50a9decd9e35d18ecde83f518
Dave Sanders <davepaulsanders@gmail.com>
2022-03-09 15:36:11 -0500

    endGame function added for win and lose

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Selectors
 const timeDisplay = document.getElementById("time-display");
-const startButton = document.getElementById("start");
+var startButton = document.getElementById("start");
 const siteTitle = document.getElementById("title");
 var questionText = document.getElementById("questions");
 var answerBox = document.getElementById("question-ans-container");
-let questionNumber = 0;
+let questionCount = 0;
 const questionButton = document.getElementById("question-choice");
 
 // initializing choice variables and adding them to an array
@@ -27,7 +27,7 @@ const questionSetUp = () => {
 const quizQuestions = [
   ["What's my name?", "Dave", "Kevin", "James", "Meg", "Dave"],
   ["What's my wife's name?", "Jeff", "Maggie", "Jess", "Evan", "Maggie"],
-  ["Question 3", "b", "b", "c", "d", "a"],
+  ["Question 3", "a", "b", "c", "d", "a"],
   ["Question 4", "c", "b", "c", "d", "b"],
 ];
 let count = 0;
@@ -35,13 +35,22 @@ timeDisplay.textContent = `Time: ${count}`;
 
 // Starts the quiz by starting timer, clearning page and adding first question
 const startQuiz = () => {
+  // reset counter if played before
+  count = 0;
+  questionCount = 0;
   timer();
   questionSetUp();
-  questionChange(questionNumber);
+  questionChange(questionCount);
 };
 
 // takes in the question number and populates the question and choices
 const questionChange = (num) => {
+  if (num === quizQuestions.length) {
+    endGame();
+    return;
+  }
+  // resetting question font-size if loss happened previously
+  questionText.style = "font-size: 1.25rem;";
   questionText.textContent = quizQuestions[num][0];
   for (let i = 0; i < quizQuestions.length; i++) {
     choiceArr[i].textContent = quizQuestions[num][i + 1];
@@ -50,13 +59,12 @@ const questionChange = (num) => {
 
 //timer for quiz
 const timer = () => {
-  count = 1000;
+  count = 75;
   timeDisplay.textContent = `Time: ${count}`;
   const gameTime = setInterval(function () {
     if (count === 0) {
-      questionText.textContent = "You lose!";
-      startButton.textContent = "Try again?";
       clearInterval(gameTime);
+      endGame();
       return;
     }
     count--;
@@ -68,21 +76,48 @@ startButton.addEventListener("click", function () {
   startQuiz();
 });
 
-var clicked = document.body.addEventListener("click", (event) => {
+// What happens if game ends under any condition
+const endGame = () => {
+  // get rid of choices
+  for (let i = 0; i < quizQuestions.length; i++) {
+    choiceArr[i].remove();
+  }
+
+  if (count > 0 && questionCount === quizQuestions.length) {
+    questionText.style = "font-size: 3rem;";
+    questionText.textContent = "You win!";
+    startButton.style = "display: block;";
+    startButton.textContent = "Try again?";
+    // This doesn't allow restart for some reason
+  } else {
+    questionText.style = "font-size: 3rem;";
+    questionText.textContent = "You lose!";
+    startButton.style = "display: block;";
+    startButton.textContent = "Try again?";
+  }
+};
+
+// What to do with player answers
+const playerAnswer = (event) => {
   if (
     event.target.className === "questionChoice" &&
-    event.target.textContent === quizQuestions[questionNumber][5]
+    event.target.textContent === quizQuestions[questionCount][5]
   ) {
-    questionNumber++;
-    questionChange(questionNumber);
+    questionCount++;
+    questionChange(questionCount);
   } else if (
     event.target.className === "questionChoice" &&
-    event.target.textContent !== quizQuestions[questionNumber][5]
+    event.target.textContent !== quizQuestions[questionCount][5]
   ) {
     if (count < 10) {
-      questionText.textContent = "You lose!";
+      endGame();
     } else {
       count = count - 10;
     }
   }
+};
+
+// listening for clicks on document body
+var clicked = document.body.addEventListener("click", (event) => {
+  playerAnswer(event);
 });
