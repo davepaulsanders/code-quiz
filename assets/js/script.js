8e1bb29e72115b751692a006a8b98f9f615faac4
Dave Sanders <davepaulsanders@gmail.com>
2022-03-09 10:19:55 -0500

    Set up quiz question data structure and startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 // Selectors
 const timeDisplay = document.getElementById("time-display");
 const startButton = document.getElementById("start");
+const siteTitle = document.getElementById("title");
+const questionText = document.getElementById("questions");
+
+const quizQuestions = [
+  ["Question 1", "a", "b", "c", "d", "c"],
+  ["Question 2", "a", "b", "c", "d", "d"],
+  ["Question 3", "a", "b", "c", "d", "a"],
+  ["Question 4", "a", "b", "c", "d", "b"],
+];
 let count = 0;
 timeDisplay.textContent = `Time: ${count}`;
 
+const startQuiz = () => {
+  siteTitle.textContent = "";
+  for (let i = 0; i < quizQuestions.length; i++) {
+    questionText.textContent = quizQuestions[i][0];
+  }
+};
 const timer = () => {
+  count = 75;
+  timeDisplay.textContent = `Time: ${count}`;
   const gameTime = setInterval(function () {
     if (count === 0) {
       clearInterval(gameTime);
@@ -16,7 +33,6 @@ const timer = () => {
   }, 1000);
 };
 startButton.addEventListener("click", function () {
-  count = 75;
-  timeDisplay.textContent = `Time: ${count}`;
+  startQuiz();
   timer();
 });
