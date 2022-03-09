96ad5116245ecf0c8747c14e1c14e9ecdc318000
Dave Sanders <davepaulsanders@gmail.com>
2022-03-09 14:53:06 -0500

    Refactored quiz functions to only create buttons once and then change text content for each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,59 @@ const timeDisplay = document.getElementById("time-display");
 const startButton = document.getElementById("start");
 const siteTitle = document.getElementById("title");
 var questionText = document.getElementById("questions");
+var answerBox = document.getElementById("question-ans-container");
+let questionNumber = 0;
+const questionButton = document.getElementById("question-choice");
 
+// initializing choice variables and adding them to an array
+let choiceA, choiceB, choiceC, choiceD;
+let choiceArr = [choiceA, choiceB, choiceC, choiceD];
+
+// removes title, creates choice buttons and adds them to HTML
+const questionSetUp = () => {
+  siteTitle.textContent = "";
+  startButton.style = "display: none";
+  for (let i = 0; i < choiceArr.length; i++) {
+    choiceArr[i] = document.createElement("button");
+    choiceArr[i].className = "questionChoice";
+    choiceArr[i].setAttribute("id", "question-choice");
+    answerBox.appendChild(choiceArr[i]);
+  }
+};
+
+// this accesses the right element, but it's not a function which is an issue
 const quizQuestions = [
-  ["Question 1", "a", "b", "c", "d", "c"],
-  ["Question 2", "a", "b", "c", "d", "d"],
-  ["Question 3", "a", "b", "c", "d", "a"],
-  ["Question 4", "a", "b", "c", "d", "b"],
+  ["What's my name?", "Dave", "Kevin", "James", "Meg", "Dave"],
+  ["What's my wife's name?", "Jeff", "Maggie", "Jess", "Evan", "Maggie"],
+  ["Question 3", "b", "b", "c", "d", "a"],
+  ["Question 4", "c", "b", "c", "d", "b"],
 ];
 let count = 0;
 timeDisplay.textContent = `Time: ${count}`;
 
+// Starts the quiz by starting timer, clearning page and adding first question
 const startQuiz = () => {
   timer();
-  siteTitle.textContent = "";
-  questionText.textContent = quizQuestions[0][0];
+  questionSetUp();
+  questionChange(questionNumber);
 };
+
+// takes in the question number and populates the question and choices
+const questionChange = (num) => {
+  questionText.textContent = quizQuestions[num][0];
+  for (let i = 0; i < quizQuestions.length; i++) {
+    choiceArr[i].textContent = quizQuestions[num][i + 1];
+  }
+};
+
+//timer for quiz
 const timer = () => {
-  count = 75;
+  count = 1000;
   timeDisplay.textContent = `Time: ${count}`;
   const gameTime = setInterval(function () {
     if (count === 0) {
       questionText.textContent = "You lose!";
+      startButton.textContent = "Try again?";
       clearInterval(gameTime);
       return;
     }
@@ -34,4 +66,23 @@ const timer = () => {
 };
 startButton.addEventListener("click", function () {
   startQuiz();
+});
+
+var clicked = document.body.addEventListener("click", (event) => {
+  if (
+    event.target.className === "questionChoice" &&
+    event.target.textContent === quizQuestions[questionNumber][5]
+  ) {
+    questionNumber++;
+    questionChange(questionNumber);
+  } else if (
+    event.target.className === "questionChoice" &&
+    event.target.textContent !== quizQuestions[questionNumber][5]
+  ) {
+    if (count < 10) {
+      questionText.textContent = "You lose!";
+    } else {
+      count = count - 10;
+    }
+  }
 });
