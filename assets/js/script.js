8c417d636a463004aea73857b09b262baf9b9003
Dave Sanders <davepaulsanders@gmail.com>
2022-03-09 09:54:22 -0500

    Timer functioning on start button click and displayed on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+// Selectors
+const timeDisplay = document.getElementById("time-display");
+const startButton = document.getElementById("start");
+let count = 0;
+timeDisplay.textContent = `Time: 0`;
 
+const timer = () => {
+  const gameTime = setInterval(function () {
+    if (count === 0) {
+      clearInterval(gameTime);
+      return false;
+    }
+    count--;
+    timeDisplay.textContent = `Time: ${count}`;
+    //timeDisplay.textContent = `Time: ${count}`
+  }, 1000);
+};
+startButton.addEventListener("click", function () {
+  count = 75;
+  timeDisplay.textContent = `Time: ${count}`;
+  timer();
+});
